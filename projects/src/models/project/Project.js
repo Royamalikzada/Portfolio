639a0f66243fb8d8377f3fee3526ae450c7b4aab
Royamalikzada <royamelikzade@gmail.com>
2022-05-05 18:37:44 +0200

    Component Projects and model Project created

---
+++ projects/src/models/project/Project.js
@@ -0,0 +1,15 @@
+export default class Project {
+    constructor (title, image, technologies) {
+        this.title = title;
+        this.image = image;
+        this.technolgies = technologies;
+    }
+
+    createProject() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
